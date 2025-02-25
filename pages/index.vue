a4fc814c8b5c97803cbaad64b857bab647829210
psa2129 <sanga2129@gmail.com>
2025-02-26 00:36:20 +0900

    Add files via upload

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <MAINPAGE />
+</template>
+
+<script setup>
+import MAINPAGE from "@components/organisms/index.vue";
+</script>
+
+<style lang="scss" scoped>
+@import "@assets/styles/main.scss";
+
+.page {
+    width: 100vw;
+    height: 100vh;
+
+    background-color: $color-white-100;
+}
+</style>
